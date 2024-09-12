d59f1dce9c879be371f8df936058aeb71654bf41
rajariandhana <rajariandhana@gmail.com>
2024-09-12 21:54:15 +0700

    com

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <x-slot:title>{{$title}}</x-slot:title>
+    <h3>ini adalah halaman </h3>
+    {{-- @dump($posts) --}}
+    @foreach ($posts as $post)   
+        <article class="max-w-screen-md py-8 border-b border-gray-500">
+            <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900 hover:underline">{{$post['title']}}</h2>
+            <div class="text-base text-gray-500">
+                <a href="">{{$post['author']}}</a> | {{$post['date']}}
+            </div>
+            <p class="my-4 font-light">
+                {{-- {{Str::limit($post['body'],100)}} --}}
+                {{$post['body']}}
+            </p>
+            {{-- href="/posts/{{$id}}"  --}}
+            <a href="/posts/1" class="font-medium text-blue-500">Read more &raquo;</a>
+        </article>
+    @endforeach
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,31 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('home',[
         'title'=>'Home Page'
     ]);
 });
-Route::get('/blog', function () {
-    return view('blog',[
-        'title'=>'Blog'
-    ]);
-});
+// Route::get('/posts', function () {
+//     return view('posts',[
+//         'title'=>'Blog',
+//         'posts'=> PostController::GetPosts()
+//     ]);
+// });
+
+Route::get('/posts',[PostController::class,'index']);
+// Route::get('/posts/{id}',function($id){
+//     return view('post',[
+//         'title'=>'Blog',
+//         'post'=>Arr::first($posts, function($post) use($id){
+//             return $post['id']==$id;
+//         });
+//     ]);
+// });
+
 Route::get('/about', function () {
     return view('about',[
         'title'=>'About'
