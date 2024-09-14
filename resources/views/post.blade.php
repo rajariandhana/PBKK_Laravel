64f4f6efba1196d2367a25e8f561191f95b5de00
rajariandhana <rajariandhana@gmail.com>
2024-09-14 16:35:56 +0700

    com

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>{{$title}}</x-slot:title>
+
+    <article class="max-w-screen-md py-8 border-b border-gray-500">
+        <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900 hover:underline">{{$post['title']}}</h2>
+        <div class="text-base text-gray-500">
+            <a href="">{{$post['author']}}</a> | {{$post['date']}}
+        </div>
+        <p class="my-4 font-light">
+            {{-- {{Str::limit($post['body'],100)}} --}}
+            {{$post['body']}}
+        </p>
+        {{-- href="/posts/{{$id}}"  --}}
+        <a href="/posts" class="font-medium text-blue-500">&laquo; Back to posts</a>
+    </article>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,29 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home',[
         'title'=>'Home Page'
     ]);
 });
-// Route::get('/posts', function () {
-//     return view('posts',[
-//         'title'=>'Blog',
-//         'posts'=> PostController::GetPosts()
-//     ]);
-// });
 
-Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts', function () {
+    return view('posts',[
+        'title'=>'Blog',
+        'posts'=> Post::all()
+    ]);
+});
+Route::get('/posts/{slug}',function($slug){
+    $post = Post::find($slug);
+    return view('post',[
+        'title'=>'Single Post',
+        'post'=>$post
+    ]);
+});
+
+// Route::get('/posts',[PostController::class,'index']);
 // Route::get('/posts/{id}',function($id){
 //     return view('post',[
 //         'title'=>'Blog',
