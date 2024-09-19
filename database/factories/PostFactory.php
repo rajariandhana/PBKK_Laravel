14f7eb4a4a703371abac38017dcf2bcea03cddf8
rajariandhana <rajariandhana@gmail.com>
2024-09-19 14:19:42 +0700

    com

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -21,8 +22,10 @@ class PostFactory extends Factory
         return [
             'title'=>fake()->sentence(),
             'author_id'=>User::factory(),
+            'category_id'=>Category::factory(),
             'slug'=>Str::slug(fake()->sentence()),
             'body'=>fake()->text()
         ];
     }
-}
+    //App\Models\Post::factory(100)->recycle([Category::factory(3)->create(),User::factory(3)->create(),User::factory(5)->create()])->create();
+} 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,8 +5,11 @@
     @foreach ($posts as $post)   
         <article class="max-w-screen-md py-8 border-b border-gray-500">
             <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900 hover:underline">{{$post['title']}}</h2>
-            <div class="text-base text-gray-500">
-                <a href="/authors/{{$post->author->id}}" class="hover:underline">{{$post->author->name}}</a> 
+            <div class="">
+                By
+                <a href="/authors/{{$post->author->username}}" class="text-base text-gray-500 hover:underline">{{$post->author->name}}</a> 
+                in
+                <a href="/categories/{{$post->category->slug}}" class="text-base text-gray-500 hover:underline">{{$post->category->name}}</a> 
                 | {{$post->created_at->format('j F Y')}}
             </div>
             <p class="my-4 font-light">
